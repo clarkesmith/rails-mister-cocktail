429db1663a9d555a98e8fc2b96981ff64b95c3de
Ben Smith <bsmith20@dcs.bbk.ac.uk>
2018-08-17 17:15:01 +0100

    some css

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'cocktails/doses'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "cocktails#index"
+
+  resources :cocktails, only: [:edit, :show, :index, :new, :create, :destroy] do
+    resources :doses
+  end
 end
